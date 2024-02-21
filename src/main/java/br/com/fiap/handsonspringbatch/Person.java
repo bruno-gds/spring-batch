5df479e38d9cd18255eb94158c99f87cecf823d3
bruno-gds <brunog.damascena@gmail.com>
2024-02-21 20:01:36 -0300

    criando domínio Person

---
+++ src/main/java/br/com/fiap/handsonspringbatch/Person.java
@@ -0,0 +1,84 @@
+package br.com.fiap.handsonspringbatch;
+
+import java.time.LocalDateTime;
+
+/**
+ * @author Bruno Gomes Damascena dos santos (bruno-gds) < brunog.damascena@gmail.com >
+ * Date: 20/02/2024
+ * Project Name: spring-batch
+ */
+
+public class Person {
+    private String name;
+    private String streetName;
+    private String number;
+    private String city;
+    private String country;
+    private String email;
+    private String phoneNumber;
+    private LocalDateTime createDateTime;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getStreetName() {
+        return streetName;
+    }
+
+    public void setStreetName(String streetName) {
+        this.streetName = streetName;
+    }
+
+    public String getNumber() {
+        return number;
+    }
+
+    public void setNumber(String number) {
+        this.number = number;
+    }
+
+    public String getCity() {
+        return city;
+    }
+
+    public void setCity(String city) {
+        this.city = city;
+    }
+
+    public String getCountry() {
+        return country;
+    }
+
+    public void setCountry(String country) {
+        this.country = country;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getPhoneNumber() {
+        return phoneNumber;
+    }
+
+    public void setPhoneNumber(String phoneNumber) {
+        this.phoneNumber = phoneNumber;
+    }
+
+    public LocalDateTime getCreateDateTime() {
+        return createDateTime;
+    }
+
+    public void setCreateDateTime(LocalDateTime createDateTime) {
+        this.createDateTime = createDateTime;
+    }
+}
